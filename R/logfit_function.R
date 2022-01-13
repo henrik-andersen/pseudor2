71bfe0669a7e71959927ea2037f2ed7372a14fc7
henrik-andersen <henrik.andersen@soziologie.tu-chemnitz.de>
2022-01-13 11:29:49 +0100

    Fixed an error that only let models called lm1 be used

--- R/logfit_function.R
+++ R/logfit_function.R
@@ -1,6 +1,7 @@
 #' A function to get some common pseudo R2 measures from a glm
 #' 
 #' This function takes a fitted glm object and returns some common psuedo R2 measures.
+#' It currently returns the McFadden, Cox and Snell and Nagelkerke pseudo R square.
 #' @param model The fitted glm object
 #' @keywords glm
 #' @export
@@ -23,18 +24,18 @@ logfit <- function(model) {
   } 
   
   # McFadden's R2
-  llp <- -2^-1 * lm1$deviance
-  llo <- -2^-1 * lm1$null.deviance
+  llp <- -2^-1 * model$deviance
+  llo <- -2^-1 * model$null.deviance
   mcfadden <- 1 - (llp / llo)
   
   # Cox and Snell R2
-  G <- lm1$null.deviance - lm1$deviance
-  n <- dim(lm1$model)[1]
+  G <- model$null.deviance - model$deviance
+  n <- dim(model$model)[1]
   
   coxsnell <- 1 - exp(-G / n)
   
   # Nagelkerke R2
-  nagelkerke <- coxsnell / (1 - exp(-(lm1$null.deviance / n)))
+  nagelkerke <- coxsnell / (1 - exp(-(model$null.deviance / n)))
   
   # Return the pseuod R2s
   return(list("McFaddens R2" = mcfadden, 
